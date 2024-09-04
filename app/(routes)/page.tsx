b7603a4eee365972e53e6bb2395be1dfa6b233c5
huynth1801 <huuhuy1801@gmail.com>
2024-09-04 23:35:15 +0700

    display product detail

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,24 @@
-const HomePage = () => {
-  return <div>Hello Store</div>;
+import getProduct from "@/actions/get-products";
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "../components/billboard";
+import Container from "../components/ui/container";
+import ProductList from "../components/product-list";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProduct({ isFeatured: true });
+  const billboard = await getBillboard("559ee6c1-a029-4ca8-b3d7-44b2a91d2974");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={products} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Container from "./ui/container";
 import MainNav from "./main-nav";
 import getCategories from "@/actions/get-categories";
+import NavbarActions from "./navbar-actions";
 
 export const revalidate = 0;
 
@@ -11,13 +12,11 @@ const Navbar = async () => {
     <div className="border-b">
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
-          <Link
-            href={"/"}
-            className="ml-4 flex lg:ml-0 gap-x-2"
-          >
+          <Link href={"/"} className="ml-4 flex lg:ml-0 gap-x-2">
             <p className="font-bold text-xl">STORE</p>
           </Link>
           <MainNav data={categories} />
+          <NavbarActions />
         </div>
       </Container>
     </div>
